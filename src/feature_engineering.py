c76677cb14b1504bb82bb3aa223b0d1b371f96a1
Anurag P. <153854036+Anurag-2601@users.noreply.github.com>
2026-01-20 17:13:46 +0530

    Update feature_engineering.py

--- src/feature_engineering.py
+++ src/feature_engineering.py
@@ -1,1 +1,7 @@
+from sklearn.decomposition import PCA
 
+def apply_pca(X_train, X_test, variance=0.95):
+    pca = PCA(n_components=variance)
+    X_train_pca = pca.fit_transform(X_train)
+    X_test_pca = pca.transform(X_test)
+    return X_train_pca, X_test_pca, pca
