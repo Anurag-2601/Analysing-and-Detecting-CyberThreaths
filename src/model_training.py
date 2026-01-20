10d4a709a17b333c722c17da5428a7941228b488
Anurag P. <153854036+Anurag-2601@users.noreply.github.com>
2026-01-20 17:14:53 +0530

    Update model_training.py

--- src/model_training.py
+++ src/model_training.py
@@ -1,1 +1,18 @@
+def train_autoencoder(model, X_train):
+    model.fit(
+        X_train, X_train,
+        epochs=30,
+        batch_size=64,
+        verbose=1
+    )
+    return model
+
+def train_dnn(model, X_train, y_train):
+    model.fit(
+        X_train, y_train,
+        epochs=30,
+        batch_size=32,
+        verbose=1
+    )
+    return model
 
